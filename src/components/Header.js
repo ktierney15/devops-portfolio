be27f4bd4c1ea8ba710d455a0dfeaf053881c7c2
Kevin Tierney <kevintierney@Kevins-Air-2.fios-router.home>
2023-03-16 09:54:05 -0400

    created a header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function Header() {
+    return (
+        <div className="head-bar">
+          <p>
+            Kevin Tierney
+          </p>
+          <div>
+            <a>Projects</a>
+            <a>Resume</a>
+            <a>About Me</a>
+          </div>
+        </div>
+    )
+}
