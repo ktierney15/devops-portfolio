4fea322a8795591ea4b0d1dee43904e386799179
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2023-12-18 20:12:21 -0500

    added some resume sections

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,35 @@ import React from "react"
 export default function Resume() {
     return (
         <div>
-            <p>resume</p>
+            <h1>Skills</h1>
+            <ul>
+                <li>Continuous Integration and Continuous Delivery (CI/CD)</li>
+                <p>Set up many different types of applciations with CI/CD pipelines in Jenkins and Github actions</p>
+                <li>Github Actions</li>
+                <li>Terraform</li>
+                <li>Python</li>
+                <li>Amazon Web Services (AWS)</li>
+                <li>Ansible</li>
+            </ul>
+
+            <h1>Expereince</h1>
+
+            <h1>Education</h1>
+            <p>Elon University (2018-2022) : Bachelor of Science - Computer Science </p>
+
+            <h1>Certifications</h1>
+            <ul>
+                <li>Splunk</li>
+                <li>AWS....</li>
+            </ul>
+
+            <h1>Links</h1>
+            <ul>
+                <li>Github</li>
+                <li>Stack overflow</li>
+            </ul>
+            
+            <p></p>
         </div>
     )
 }
