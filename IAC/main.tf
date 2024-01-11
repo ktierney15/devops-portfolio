bb9f84b327b798f6bd5d8dcf81afd36eca974ff7
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-01-10 20:20:44 -0500

    added terraform test

--- IAC/main.tf
+++ IAC/main.tf
@@ -2,24 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-xxxxxxxxxxxxxxxxx"
+resource "aws_instance" "test" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
-  key_name      = "your-key-pair-name"
-  subnet_id     = "subnet-xxxxxxxxxxxxxxxxx" # Specify your subnet ID
-
-  security_group_names = ["your-security-group-name"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              # Your user data script here
-              EOF
-
-  tags = {
-    Name = "example-instance"
-  }
 }
 
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
+
+
+
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_instance" "example" {
+#   ami           = "ami-xxxxxxxxxxxxxxxxx"
+#   instance_type = "t2.micro"
+#   key_name      = "your-key-pair-name"
+#   subnet_id     = "subnet-xxxxxxxxxxxxxxxxx" # Specify your subnet ID
+
+#   security_group_names = ["your-security-group-name"]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               # Your user data script here
+#               EOF
+
+#   tags = {
+#     Name = "example-instance"
+#   }
+# }
+
+# output "instance_public_ip" {
+#   value = aws_instance.example.public_ip
+# }
