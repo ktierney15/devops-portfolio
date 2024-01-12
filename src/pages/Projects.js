0306c363d4d4eac0685b789c04cbd703d450d07a
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-01-12 17:52:04 -0500

    fixed build errors

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ export default function Projects() {
                 {projects.projects.map((project, index) => (
                 <li key={index}>
                     <h3>{project.name}</h3>
-                    <img src={project.image} alt={`${project.name} Image`} style={{ maxWidth: '100%' }} />
+                    <img src={project.image} alt={`${project.name}`} style={{ maxWidth: '100%' }} />
                     <p>Description: {project.description}</p>
                     <p>
                     GitHub: <a href={project.githubLink} target="_blank" rel="noopener noreferrer">{project.githubLink}</a>
