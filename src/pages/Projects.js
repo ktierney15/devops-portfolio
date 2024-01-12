e731feff5af1e24f5cc6f9715751edf1d532f20e
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-01-12 18:36:01 -0500

    styled projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,22 +1,37 @@
-import React from "react"
-import projects from '../projects.json';
+import React from 'react';
+import projectsData from '../projects.json';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
-export default function Projects() {
+const Projects = () => {
     return (
-        <div>
-            <p>projects</p>
-            <ul>
-                {projects.projects.map((project, index) => (
-                <li key={index}>
-                    <h3>{project.name}</h3>
-                    <img src={project.image} alt={`${project.name}`} style={{ maxWidth: '100%' }} />
-                    <p>Description: {project.description}</p>
-                    <p>
-                    GitHub: <a href={project.githubLink} target="_blank" rel="noopener noreferrer">{project.githubLink}</a>
-                    </p>
-                </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+      <div style={{ padding: '16px' }}>
+        <Typography variant="h2">Projects</Typography>
+        <ul style={{ listStyleType: 'none', padding: 0 }}>
+          {projectsData.projects.map((project, index) => (
+            <li key={index} style={{ marginBottom: '16px' }}>
+              <Typography variant="h6" style={{ fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '8px' }}>
+                {project.name}
+              </Typography>
+              <img
+                src={project.image}
+                alt={`${project.name}`}
+                style={{ maxWidth: '100%', marginBottom: '8px' }}
+              />
+              <Typography variant="body1" style={{ marginBottom: '8px' }}>
+                Description: {project.description}
+              </Typography>
+              <Typography variant="body1">
+                GitHub:{' '}
+                <Link href={project.githubLink} target="_blank" rel="noopener noreferrer" style={{ color: '#1976D2' }}>
+                  {project.githubLink}
+                </Link>
+              </Typography>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Projects;
