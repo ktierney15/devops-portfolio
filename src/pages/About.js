81277c55b7af672acaa693316b0cbca5a65acf72
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-01-12 17:43:55 -0500

    added build to ci

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,9 @@ import React from "react"
 const About = () =>{
     return (
         <div>
-            <p>about</p>
+            {/* Picture of myself right here */}
+            <p>My name is Kevin Tierney and I am a Software Engineer whos specializes in DevOps. </p>
+            {/* further go into your story, how you discovered comp sci, how you discovered devops etc */}
         </div>
     )
 }
