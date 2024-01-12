fc142eb3865a7f69ad624135b2f5380f78f6a7a7
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-01-12 18:50:11 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,3 @@
-// import React from "react"
-
-// const About = () =>{
-//     return (
-//         <div>
-//             {/* Picture of myself right here */}
-//             <p>My name is Kevin Tierney and I am a Software Engineer whos specializes in DevOps. </p>
-//             {/* further go into your story, how you discovered comp sci, how you discovered devops etc */}
-//         </div>
-//     )
-// }
-
-// export default About
-
-
 import React from 'react';
 
 const AboutMe = () => {
