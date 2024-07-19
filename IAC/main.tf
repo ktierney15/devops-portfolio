a4814e606d013a63ca77c3d37f36b45341d3e7f0
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-07-19 14:31:25 -0400

    fix bucket policy

--- IAC/main.tf
+++ IAC/main.tf
@@ -43,6 +43,14 @@ resource "aws_s3_bucket" "bucket" {
     index_document = "index.html"
     error_document = "index.html"
   }
+
+  public_access_block_configuration {
+    block_public_acls = false
+    ignore_public_acls = false
+    block_public_policy = false
+    restrict_public_buckets = false
+  }
+  
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
