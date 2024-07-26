eadcb579a057545fd06c3d107953cacee6afda87
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-07-26 14:16:01 -0400

    add dns

--- IAC/main.tf
+++ IAC/main.tf
@@ -81,7 +81,7 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket.bucket.website_endpoint #"kt15-${var.app_name}.s3-website-us-east-1.amazonaws.com"
+    name                   = "kt15-${var.app_name}.s3-website-us-east-1.amazonaws.com"
     zone_id                = aws_s3_bucket.bucket.hosted_zone_id
     evaluate_target_health = false
   }
@@ -93,7 +93,7 @@ resource "aws_route53_record" "root" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket.bucket.website_endpoint # "kt15-${var.app_name}.s3-website-us-east-1.amazonaws.com" # aws_s3_bucket.bucket.bucket_domain_name
+    name                   = "kt15-${var.app_name}.s3-website-us-east-1.amazonaws.com" # aws_s3_bucket.bucket.bucket_domain_name
     zone_id                = aws_s3_bucket.bucket.hosted_zone_id
     evaluate_target_health = false
   }
