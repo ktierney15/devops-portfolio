7e6f03ae5e1719b1e9d0f2fd7db450060c05aec6
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-07-27 11:10:24 -0400

    test

--- IAC/main.tf
+++ IAC/main.tf
@@ -81,7 +81,7 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = "kt15-${var.app_name}.s3-website.us-east-1.amazonaws.com"
+    name                   = aws_s3_bucket_website_configuration.website.website_domain #"kt15-${var.app_name}.s3-website.us-east-1.amazonaws.com"
     zone_id                = aws_s3_bucket.bucket.hosted_zone_id
     evaluate_target_health = false
   }
@@ -93,7 +93,7 @@ resource "aws_route53_record" "root" {
   type    = "A"
 
   alias {
-    name                   = "kt15-${var.app_name}.s3-website.us-east-1.amazonaws.com" # aws_s3_bucket.bucket.bucket_domain_name
+    name                   = aws_s3_bucket_website_configuration.website.website_domain  # "kt15-${var.app_name}.s3-website.us-east-1.amazonaws.com"
     zone_id                = aws_s3_bucket.bucket.hosted_zone_id
     evaluate_target_health = false
   }
@@ -104,14 +104,14 @@ resource "aws_route53_record" "root" {
 #   private_zone = false
 # }
 
-# resource "aws_s3_bucket_website_configuration" "website" {
-#   bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
 
-#   index_document {
-#     suffix = "index.html"
-#   }
+  index_document {
+    suffix = "index.html"
+  }
 
-#   error_document {
-#     key = "index.html"
-#   }
-# }
+  error_document {
+    key = "index.html"
+  }
+}
