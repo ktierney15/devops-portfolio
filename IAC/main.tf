23279146488b6dd9bb50423cc857f26b93366c9b
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-08-10 10:23:40 -0400

    delete cert

--- IAC/main.tf
+++ IAC/main.tf
@@ -170,27 +170,27 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 
 
 # route 53
-resource "aws_route53_record" "www" {
-  zone_id = var.route53_zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
+# resource "aws_route53_record" "www" {
+#   zone_id = var.route53_zone_id
+#   name    = "www.${var.domain_name}"
+#   type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.cdn.domain_name
-    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.cdn.domain_name
+#     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
 
-resource "aws_route53_record" "root" {
-  zone_id = var.route53_zone_id
-  name    = var.domain_name
-  type    = "A"
+# resource "aws_route53_record" "root" {
+#   zone_id = var.route53_zone_id
+#   name    = var.domain_name
+#   type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.cdn.domain_name
-    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.cdn.domain_name
+#     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
 
