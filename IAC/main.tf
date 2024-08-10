f006a6fcac76f89faca823ad0b0ff8d6bdeff16d
ktierney15 <63060232+ktierney15@users.noreply.github.com>
2024-08-10 10:33:53 -0400

    reintroduce cf distro

--- IAC/main.tf
+++ IAC/main.tf
@@ -80,62 +80,62 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   })
 }
 
-# resource "aws_cloudfront_distribution" "cdn" {
-#   origin {
-#     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
-#     origin_id   = aws_s3_bucket.bucket.id
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.bucket.id
 
-#     s3_origin_config {
-#       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-#     }
-#   }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = aws_s3_bucket.bucket.id
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.bucket.id
 
-#     forwarded_values {
-#       query_string = false
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 
-#   # viewer_certificate {
-#   #   acm_certificate_arn            = aws_acm_certificate.cert.arn
-#   #   ssl_support_method              = "sni-only"
-#   #   minimum_protocol_version        = "TLSv1.2_2018"
-#   # }
+  # viewer_certificate {
+  #   acm_certificate_arn            = aws_acm_certificate.cert.arn
+  #   ssl_support_method              = "sni-only"
+  #   minimum_protocol_version        = "TLSv1.2_2018"
+  # }
 
-#   tags = {
-#     Name = "CloudFront for ${var.domain_name}"
-#   }
-# }
+  tags = {
+    Name = "CloudFront for ${var.domain_name}"
+  }
+}
 
-# resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-#   comment = "Access Identity for S3 bucket"
-# }
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Access Identity for S3 bucket"
+}
 
 # resource "aws_acm_certificate" "cert" {
 #   domain_name       = "www.${var.domain_name}"
@@ -170,27 +170,27 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 
 
 # route 53
-# resource "aws_route53_record" "www" {
-#   zone_id = var.route53_zone_id
-#   name    = "www.${var.domain_name}"
-#   type    = "A"
+resource "aws_route53_record" "www" {
+  zone_id = var.route53_zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
 
-#   alias {
-#     name                   = aws_cloudfront_distribution.cdn.domain_name
-#     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 
-# resource "aws_route53_record" "root" {
-#   zone_id = var.route53_zone_id
-#   name    = var.domain_name
-#   type    = "A"
+resource "aws_route53_record" "root" {
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "A"
 
-#   alias {
-#     name                   = aws_cloudfront_distribution.cdn.domain_name
-#     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 
