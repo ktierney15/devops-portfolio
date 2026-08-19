9834eb586b90322ca508020694392ed777966330
ktierney15 <kevintierney99@gmail.com>
2026-08-19 19:18:31 -0400

    cleanup dead code

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Header from "./components/Header"
 
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
-import Knowledge from "./pages/Kowledge"
-import About from "./pages/About"
 import Resume from "./pages/Resume"
 import Blog from "./pages/Blog"
 
@@ -22,8 +20,6 @@ function App() {
           <Route path="/" element={<Header />}>
             <Route index element={<Home />} />
             <Route path="projects" element={<Projects />} />
-            <Route path="knowledge-base" element={<Knowledge />} />
-            <Route path="about" element={<About />} />
             <Route path="resume" element={<Resume />} />
             <Route path="blog" element={<Blog />} />
             {/* Catch all route */}
